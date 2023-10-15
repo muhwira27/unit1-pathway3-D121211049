b9132250712df982fcce6c535e7c078be951cac9
Muh. Wira <muhwira907@gmail.com>
2023-10-15 14:38:34 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "unit1-pathway3-D121211049"
+include(":app")
+ 
