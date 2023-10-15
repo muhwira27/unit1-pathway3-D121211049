27db4e3546088fcb713e1d8dc23a22226d3e45cd
Muh. Wira <muhwira907@gmail.com>
2023-10-15 15:15:16 +0800

    Change default font to poppins

--- app/src/main/java/com/example/unit1_pathway3_d121211049/ui/theme/Type.kt
+++ app/src/main/java/com/example/unit1_pathway3_d121211049/ui/theme/Type.kt
@@ -2,14 +2,20 @@ package com.example.unit1_pathway3_d121211049.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.unit1_pathway3_d121211049.R
 
 // Set of Material typography styles to start with
+val Poppins = FontFamily(
+    Font(R.font.poppins)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
